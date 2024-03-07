1205720a4db9cf0cce179b0f48173c38caab9c80
flaviusmeghesan <flv.meghe@gmail.com>
2024-03-08 00:07:27 +0200

    completed ex4

---
+++ Laboratory1/Laboratory1/Exercise4.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+    internal class Exercise4
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Height(cm):");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Age(years):");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sex(m/f): ");
+        char sex = Convert.ToChar(Console.ReadLine());
+
+        Console.WriteLine();
+        Greutate greutate = new Greutate(); 
+
+        switch(sex)
+        {
+            case 'm':
+                {
+                    float weight = greutate.ManWeight(height, age);
+                    greutate.displayWeight(weight);
+                    break;
+                }
+            case 'f':
+                {
+                    float weight = greutate.WomenWeight(height, age);
+                    greutate.displayWeight(weight);
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("Invalid choice.");
+                    break;
+                }
+
+        }
+    }
+}
+
+class Greutate
+{
+    public float ManWeight(int height, int age)
+    {
+        return (float)(height-100-(height-150)/4) + ((age-20)/4);
+    }
+    public float WomenWeight(int height, int age) {
+        return (float)(height - 100 - ((height - 150) / 2.5)) + ((age - 20) / 6);
+    }
+
+    public void displayWeight(float weight)
+    {
+        Console.WriteLine("Your ideal weight is: "+ weight+ " kg.");
+    }
+}
--- Laboratory1/Laboratory1/obj/Debug/net8.0/Laboratory1.AssemblyInfo.cs
+++ Laboratory1/Laboratory1/obj/Debug/net8.0/Laboratory1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratory1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68dfd0bdeda202e60abbea8e21d9390099f2eb10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f0004c3a7afa7b30750a19897a47ed3a021672")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratory1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratory1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
